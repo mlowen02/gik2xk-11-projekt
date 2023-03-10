5450525231e2c0dce4e91dd37121d8f427e1a3e5
Marcus Lowén <h21marlo@du.se>
2023-03-10 10:19:11 +0100

    Removed score function from ProductModel

--- client/src/models/ProductModel.js
+++ client/src/models/ProductModel.js
@@ -45,27 +45,6 @@ export async function remove(id) {
 	return {};
 }
 
-export async function getAverageScore(id) {
-	var average = 0;
-	const scores = await getScores(id);
-	scores.forEach((score) => {
-		average += score.score;
-	});
-	average = average / scores.length;
-
-	return Math.round(average * 10) / 10;
-}
-export async function getScores(id) {
-	const result = await api.get(`/products/${id}/getScores`, {
-		data: { id },
-	});
-	if (result.status === 200) return result.data;
-
-	console.log(result.status);
-	console.log(result.data);
-	return {};
-}
-
 export async function addScore(productId, userId, score) {
 	const result = await api.post(`/products/${productId}/addScore`, {
 		userId,
