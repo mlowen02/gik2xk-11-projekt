fc4085fda2c103569fb6b462b8e15e8d1c4395b5
Marcus Lowén <marcus@lowen.se>
2023-03-13 13:14:35 +0100

    Function for Add product button (set min 0)

--- client/src/components/CartAdd.js
+++ client/src/components/CartAdd.js
@@ -1,16 +1,25 @@
 import { Button, TextField, Typography } from '@mui/material';
 
-function CartAdd() {
+function CartAdd({ onAdd }) {
+	let qty;
 	return (
 		<>
 			<Typography variant="h6">Add to Cart:</Typography>
 			<TextField
+				InputProps={{ inputProps: { min: 0 } }}
+				defaultValue={0}
 				id="outlined-basic"
 				label="Amount:"
 				variant="outlined"
 				type="number"
+				onChange={(e) => {
+					qty = e.target.value;
+					console.log(qty);
+				}}
 			/>
-			<Button variant="contained">Add product(s)</Button>
+			<Button variant="contained" onClick={() => onAdd(1, qty)}>
+				Add product(s)
+			</Button>
 		</>
 	);
 }
